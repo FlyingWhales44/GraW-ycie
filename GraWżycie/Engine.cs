99cc0e1265ee47aa27aa1b38c83e17cb8a1605c1
Arkadiusz Bartczak <arkbar97@gmail.com>
2018-03-12 20:12:10 +0100

    xaml added

--- GraWżycie/Engine.cs
+++ GraWżycie/Engine.cs
@@ -8,69 +8,82 @@ namespace GraWżycie
 {
     class Engine
     {
+        public bool[,] cell;
+        protected bool[,] buff;
+        public int warMax, warMin, oknoX, oknoY;
+
         public void game()
         {
-            while(true)
-            {
-                int licznik;
+            int licznik;
 
-                for(int j=1;j<oknoY-1;j++)
-                    for(int i=1;i<oknoX-1;i++)
+                for (int j = 1; j < oknoY - 1; j++)
+                    for (int i = 1; i < oknoX - 1; i++)
                     {
-                        licznik = 0;
 
-                        for(int z=i-1;z<i+2;z++)
-                            for(int c=j-1;c<j+2;c++)
-                        if( cell[c,z] == true && z!=i && c !=j)
-                                 licznik++;
+                        licznik=Count(i, j);
 
-                        if (cell[j, i] == false && licznik == warMax)
-                            buff[j, i] = true;
-                        else
-                            buff[j, i] = false;
+                        CellDie(licznik, i, j);
 
-                        if (cell[j, i] == true)
-                            if (licznik >= warMin && licznik <= warMax)
-                                buff[j, i] = true;
-                            else
-                                buff[j, i] = false;
+                        CellLife(licznik, i, j);
                     }
+                Rewrite();
 
-                for (int j = 0; j < oknoY; j++)
-                    for (int i = 0; i < oknoX; i++)
-                        cell[j, i] = buff[j, i];
-            }
+            
         }
 
-        public void set_(int x,int y,int wMax,int wMin,int oX,int oY)
+        public void CellDie(int licznik, int i, int j)
         {
+            if (cell[j, i] == false && licznik == warMax)
+                buff[j, i] = true;
+            else
+                buff[j, i] = false;
+        }
 
-            cell = new bool[x, y];
-            buff = new bool[x, y];
+        public void CellLife(int licznik, int i, int j)
+        {
+            if (cell[j, i] == true)
+                if (licznik >= warMin && licznik <= warMax)
+                    buff[j, i] = true;
+                else
+                    buff[j, i] = false;
+        }
+
+        public int Count( int i, int j)
+        {
+            int licz=0;
+            for (int z = i - 1; z < i + 2; z++)
+                for (int c = j - 1; c < j + 2; c++)
+                    if (cell[c, z] == true && z != i && c != j)
+                        licz++;
+            return licz;
+        }
+
+        public void Rewrite()
+        {
+            for (int j = 0; j < oknoY; j++)
+                for (int i = 0; i < oknoX; i++)
+                    cell[j, i] = buff[j, i];
+        }
+
+        public void set_(int x, int y, int wMax, int wMin)
+        {
+         
             warMax = wMax;
             warMin = wMin;
-            oknoX = oX;
-            oknoY = oY;
+            oknoX = x+2;
+            oknoY = y+2;
+            cell = new bool[oknoX, oknoY];
+            buff = new bool[oknoX, oknoY];
 
-            for (int i = 0; i < oY; i++)
-                for (int j = 0; j < oX; j++)
-                    cell[i,j] = false;
+            for (int i = 0; i < oknoY; i++)
+                for (int j = 0; j < oknoX; j++)
+                    cell[i, j] = false;
 
-            for (int i = 0; i < oY; i++)
-                for (int j = 0; j < oX; j++)
+            for (int i = 0; i < oknoY; i++)
+                for (int j = 0; j < oknoX; j++)
                     buff[i, j] = false;
 
         }
 
-        public void set_cells(int x,int y)
-        {
-            cell[y, x] = true;
-        }
-
-
-
-        protected bool[,] cell;
-        protected bool[,] buff;
-        protected int warMax, warMin, oknoX,oknoY;
     }
 }
--- GraWżycie/MainWindow.xaml.cs
+++ GraWżycie/MainWindow.xaml.cs
@@ -15,19 +15,112 @@ using System.Windows.Shapes;
 
 namespace GraWżycie
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        Engine engine;
+        List<Button> ButtonList;
         public MainWindow()
         {
             InitializeComponent();
+
+            engine = new Engine();
+
+            RozmiarX.TextChanged += new TextChangedEventHandler(XChanged);
+            RozmiarY.TextChanged += new TextChangedEventHandler(YChanged);
+
+            SetPlansza(Convert.ToInt32(RozmiarX.Text),Convert.ToInt32(RozmiarY.Text));
+
+            start.Click += new RoutedEventHandler(Start);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void XChanged(object sender,EventArgs e)
         {
+            var txtbox = (TextBox)sender;
+            int x = Convert.ToInt32(txtbox.Text);
 
+            SetPlansza(x, Convert.ToInt32(RozmiarY.Text));
         }
+
+        private void YChanged(object sender, EventArgs e)
+        {
+            var txtbox = (TextBox)sender;
+            int y = Convert.ToInt32(txtbox.Text);
+
+            SetPlansza(Convert.ToInt32(RozmiarX.Text), y);
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            engine.game();
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            int x=-1, y=-1;
+            var button = (Button)sender;
+
+            if (button.Background == Brushes.DarkBlue)
+                button.Background = Brushes.Gray;
+            else
+                button.Background = Brushes.DarkBlue;
+
+            GetCord(button.Name,out x,out y);
+
+            engine.cell[x, y] =! engine.cell[x,y];
+        }
+
+        private void ConvertCells()
+        {
+            Button temp;
+            for(int i=1;i<engine.oknoY-1;i++)
+                for(int j=1;j<engine.oknoX-1;j++)
+                {
+                    temp=ButtonList.SingleOrDefault(r => r.Name == "I" + j + "I" + i);
+                    if (temp != null)
+                        temp.Background = Brushes.Gray;
+
+                }
+        }
+
+        private void GetCord(string name,out int x,out int y)
+        {
+            string[] Cord = name.Split('I');
+            x = Convert.ToInt32(Cord[1]);
+            y = Convert.ToInt32(Cord[2]);
+        }
+
+        private void SetPlansza(int x,int y)
+        {
+            Button pole;
+            ButtonList = new List<Button>();
+            plansza.RowDefinitions.Clear();
+            plansza.ColumnDefinitions.Clear();
+            plansza.Children.Clear();
+
+            for (int j = 0; j < x; j++)
+                plansza.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int i = 0; i < y; i++)
+                plansza.RowDefinitions.Add(new RowDefinition());
+
+            for(int i=1;i<y+1;i++)
+                for(int j=1;j<x+1;j++)
+                {
+                    pole = new Button();
+                    pole.Background = Brushes.Gray;
+                    pole.Name = "I" + j + "I" + i;
+                    pole.SetValue(Grid.RowProperty, i-1);
+                    pole.SetValue(Grid.ColumnProperty, j-1);
+                    pole.Click+=new RoutedEventHandler(OnClick);
+                    pole.Content = pole.Name;
+                    ButtonList.Add(pole);
+
+                    plansza.Children.Add(pole);
+                }
+
+            engine.set_(x, y, 3, 3);
+        }
+
     }
 }
